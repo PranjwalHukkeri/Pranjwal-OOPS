a0b8d45f3c09a613b3614dbdf4771f45b321ebd1
PranjwalHukkeri <pranjwalhukkeri@gmail.com>
2026-04-26 23:58:06 +0530

    Add files via upload

---
+++ friendclass.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Base{
+int x;
+public:
+Base()
+{
+x=5;
+}
+friend class Derived;
+};
+class Derived{
+public:
+void display(Base b){
+cout<<"Value="<<b.x<<endl;
+}
+};
+int main()
+{
+Base b;
+Derived d;
+d.display(b);
+return 0;
+}
