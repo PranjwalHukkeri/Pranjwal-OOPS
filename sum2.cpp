c1d959fdc3c0c08406d32008f3cfa94dcc44d6db
PranjwalHukkeri <pranjwalhukkeri@gmail.com>
2026-04-26 23:59:42 +0530

    Add files via upload

---
+++ sum2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"Enter first  and second number :"<<endl;
+    cin>>num1>>num2;
+    cout<<"sum:";
+    cout<<num1+num2;
+    return 0;
+}
