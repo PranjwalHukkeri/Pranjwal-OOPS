583173316c9bc7c9aa9bc4c391dbfcbee7a3e813
PranjwalHukkeri <pranjwalhukkeri@gmail.com>
2026-04-27 00:01:55 +0530

    Add files via upload

---
+++ scope.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int m=10;
+int main()
+{
+ int a=3;
+ int m=5;
+   if (a==2)
+    {
+     int m=4;
+     cout<<::m;
+
+     }
+
+     return 0;
+     }
---
+++ string1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+string  s;
+std::cout<<"Enter String"<<endl;
+std::cin>>s;
+std::cout<<"String is "<<s<<endl;
+return 0;
+}
