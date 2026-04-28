5aa7832df4a27acefbd84e7ec8be232e5b95e90a
PRANJWAL HUKKERI <pranjwalhukkeri@gmail.com>
2026-04-28 10:21:05 +0530

    Add files via upload

---
+++ overloadingclass.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Display
+{
+public:
+
+    void display(int var)
+    {
+        cout << "Integer number: " << var << endl;
+    }
+
+
+    void display(float var)
+    {
+        cout << "Float number: " << var << endl;
+    }
+
+    void display(int var1, float var2)
+    {
+        cout << "Integer number: " << var1;
+        cout << " and float number: " << var2 << endl;
+    }
+};
+
+int main()
+{
+    int a = 5;
+    float b = 5.5;
+
+    Display d;
+
+    d.display(a);
+    d.display(b);
+    d.display(a, b);
+
+    return 0;
+}
